dda066720cc323f11cf4e6e6c2c83370e7866303
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-01-08 19:46:30 +0700

    remove colors

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,6 @@
 import {PluginDefinition} from "apollo-server-core";
 import path from "path";
-import {addColors, createLogger, format, transports} from "winston";
+import {createLogger, format, transports} from "winston";
 import "winston-daily-rotate-file";
 
 export {Logger} from "winston";
@@ -30,12 +30,6 @@ export let log = createLogger({
         format.printf(
           (info) => `[${info.timestamp} ${info.level}] ${info.message}`,
         ),
-        addColors({
-          info: "bold blue",
-          warn: "italic yellow",
-          error: "bold red",
-          debug: "green",
-        }),
       ),
     }),
   ],
